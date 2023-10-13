f6a96cfd84428d2f8c59c817db5c70a6b9861dcc
LinThit27 <linthit2745@gmail.com>
2023-10-13 14:56:25 +0630

    add service,controller,route to find book content by book id

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ const cors = require("cors");
 const dotenv = require("dotenv");
 const bodyParser = require("body-parser");
 const router = require("./routers/index.js");
+const bookContentRouter = require("./routers/bookContent.js");
+
 dotenv.config();
 
 const PORT = process.env.PORT;
@@ -12,6 +14,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use("/api/v1", router);
+app.use("/api/v1", bookContentRouter);
 app.listen(PORT, function () {
   console.log("Server listening on", PORT);
 });
