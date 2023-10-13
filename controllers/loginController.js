95103763b24fec4799a1a427e877d0f54163e64a
HponeMyatThu <hponemy8tthu@gmail.com>
2023-10-13 17:10:24 +0630

    Feat:Login Logout

---
+++ controllers/loginController.js
@@ -0,0 +1,29 @@
+// const LoginService = require("../services/loginService");
+const data = require("../config/db");
+
+exports.getLogoutUser = (req, res) => {
+    const userId = req.params.userId;
+    res.clearCookie("user");
+    res.json({ message: "Logout successful" });
+  };
+  
+
+exports.postLoginUser = (req, res) => {
+  const userName = req.body.userName;
+  const password = req.body.password;
+
+  const authenticatedUser = data.user.find(
+    (user) => user.username === userName && user.password === password
+  );
+
+  if (authenticatedUser) {
+    try {
+      res.cookie("user", authenticatedUser, { secure: true });
+      res.json({ message: "login successful", user: authenticatedUser });
+    } catch (err) {
+      res.json("Error setting cookie: " + err.message);
+    }
+  } else {
+    res.json("Login Failed");
+  }
+};
--- server.js
+++ server.js
@@ -2,16 +2,22 @@ const express = require("express");
 const cors = require("cors");
 const dotenv = require("dotenv");
 const bodyParser = require("body-parser");
-const router = require("./routers/index.js");
+// const router = require("./routers/index.js");
+const cookieParser = require("cookie-parser");
+const loginRoute = require("./routers/loginRouter.js");
+
 dotenv.config();
 
 const PORT = process.env.PORT;
 const app = express();
 app.use(cors());
 app.use(bodyParser.json());
+app.use(cookieParser());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.use("/api/v1", router);
+// app.use("/api/v1", router);
+app.use("/api", loginRoute);
+
 app.listen(PORT, function () {
   console.log("Server listening on", PORT);
 });
