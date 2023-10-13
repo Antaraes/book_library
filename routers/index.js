27710a0bcbaf93157747de1e9af29b4ab696059d
shinewwaitun <shinewaitun28@gmail.com>
2023-10-13 16:20:54 +0630

    Bookmark service,controller & routes

--- routers/index.js
+++ routers/index.js
@@ -1,9 +1,12 @@
 const express = require("express");
 const data = require("../config/db");
 const router = express.Router();
+const bookmarkRoutes = require("../routers/bookmarkRoutes");
 
 router.use("/book", (req, res, next) => {
   res.json(data);
 });
+
+router.use("/bookmark", bookmarkRoutes);
 
 module.exports = router;
