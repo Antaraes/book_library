0eec87c6a1a3890e2bce316b50b5a938dcb5bac2
shinewwaitun <shinewaitun28@gmail.com>
2023-10-18 12:01:16 +0630

    Added new url for userid,added booktitle and pages array in response

--- services/bookmarkServices.js
+++ services/bookmarkServices.js
@@ -1,12 +1,60 @@
 const jsonData = require("../config/db");
+const books = jsonData.book;
 const bookmarks = jsonData.bookmark;
+const bookContents = jsonData.bookContent;
 
 function getBookmarksByUserAndBook(bookId, userId) {
-  return bookmarks.filter((bookmark) => {
-    return bookmark.bookId == bookId && bookmark.userId == userId;
+  const result = [];
+  const bookmark = bookmarks.filter(
+    (bookmark) =>
+      bookmark.bookId === Number(bookId) && bookmark.userId === Number(userId)
+  );
+  bookmark.forEach((data) => {
+    const book = books.find((book) => book.id === data.bookId);
+    const bookContent = bookContents.filter(
+      (bookContent) =>
+        bookContent.page_no === data.book_content_page_no &&
+        bookContent.bookId === data.bookId
+    );
+    const pages = [];
+    bookContent.forEach((bookContent) => pages.push(bookContent.page_no));
+    const bookmarkData = {
+      id: data.id,
+      bookId: book.id,
+      bookTitle: book.title,
+      bookmarkPages: pages,
+    };
+    result.push(bookmarkData);
   });
+  return result;
+}
+
+function getBookmarksByUser(userId) {
+  const result = [];
+  const bookmark = bookmarks.filter(
+    (bookmark) => bookmark.userId === Number(userId)
+  );
+  bookmark.forEach((data) => {
+    const book = books.find((book) => book.id === data.bookId);
+    const bookContent = bookContents.filter(
+      (bookContent) =>
+        bookContent.page_no === data.book_content_page_no &&
+        bookContent.bookId === data.bookId
+    );
+    const pages = [];
+    bookContent.forEach((bookContent) => pages.push(bookContent.page_no));
+    const bookmarkData = {
+      id: data.id,
+      bookId: book.id,
+      bookTitle: book.title,
+      bookmarkPages: pages,
+    };
+    result.push(bookmarkData);
+  });
+  return result;
 }
 
 module.exports = {
   getBookmarksByUserAndBook,
+  getBookmarksByUser,
 };
