cbd0c0389ac0dac22145d78fe3bda9f106e581ed
Min Bhone Thant <95273099+Antaraes@users.noreply.github.com>
2023-11-06 10:06:55 +0630

    Feat:Fetch contents with supabase

--- controllers/book.controller.js
+++ controllers/book.controller.js
@@ -1,12 +1,27 @@
+// const bookContentService = require("../services/bookContentService");
+
+// const getBookContentByBookIDAndContentId = (req, res) => {
+//   const { bookId, currentPage, contentId } = req.params;
+//   const data = bookContentService.getBookContentById(bookId, currentPage, contentId);
+//   console.log("Data", data);
+
+//   if (data.error) {
+//     res.status(404).json({ error: data.error });
+//     return;
+//   }
+//   res.status(200).json(data);
+// };
+
+// module.exports = {
+//   getBookContentByBookIDAndContentId,
+// };
+
 const bookContentService = require("../services/bookContentService");
 
-const getBookContentByBookIDAndContentId = (req, res) => {
+const getBookContentByBookIDAndContentId = async (req, res) => {
   const { bookId, currentPage, contentId } = req.params;
-  const data = bookContentService.getBookContentById(
-    bookId,
-    currentPage,
-    contentId
-  );
+  const data = await bookContentService.getBookContentById(bookId, currentPage, contentId);
+  console.log("Data", data);
 
   if (data.error) {
     res.status(404).json({ error: data.error });
--- services/bookContentService.js
+++ services/bookContentService.js
@@ -1,14 +1,76 @@
 const data = require("../config/db");
 const zlib = require("zlib");
+const supabase = require("../config/supabase.js");
 
-const getBookContentById = (bookId, currentPage, contentId) => {
-  const book = data.book.find((b) => b.id == bookId);
+// const getBookContentById = (bookId, currentPage, contentId) => {
+//   const book = data.book.find((b) => b.id == bookId);
+//   if (!book) {
+//     return { error: "Cannot Find book" };
+//   }
+
+//   const content = data.bookContent.filter((content) => content.bookId == bookId);
+//   const bookDetail = data.book.find((book) => book.id == bookId);
+
+//   if (content.length == 0) {
+//     return { error: "Cannot Find book content" };
+//   }
+
+//   let currentPageIndex = -1;
+//   if (contentId) {
+//     const entryWithContentId = content.find((entry) => entry.id == contentId);
+//     if (!entryWithContentId) {
+//       return { error: "Content ID not found" };
+//     }
+//     currentPageIndex = content.indexOf(entryWithContentId);
+//   } else {
+//     currentPageIndex = content.findIndex((entry) => entry.page_no == currentPage);
+//   }
+
+//   if (currentPageIndex == -1) {
+//     return { error: "Current page not found" };
+//   }
+
+//   const pagesToShow = 10;
+//   const isFirstPage = currentPageIndex == 1;
+//   const pagesBefore = isFirstPage ? 1 : 3;
+//   console.log(pagesBefore);
+//   const pagesAfter = pagesToShow - pagesBefore - 1;
+//   console.log("pageAfter", pagesAfter);
+
+//   const startPageIndex = Math.max(0, currentPageIndex - pagesBefore);
+//   const endPageIndex = Math.min(content.length - 1, currentPageIndex + pagesAfter);
+
+//   const contentInRange = content.slice(startPageIndex, endPageIndex + 1);
+
+//   const compressedContent = contentInRange.map((entry) => ({
+//     id: entry.id,
+//     bookId: entry.bookId,
+//     content: zlib.deflateSync(JSON.stringify(entry.content)).toString("base64"),
+//     // content: entry.content,
+//     page_no: entry.page_no,
+//   }));
+
+//   const bookInfoDetail = {
+//     book: book,
+//     contents: compressedContent,
+//   };
+
+//   return bookInfoDetail;
+// };
+
+// module.exports = {
+//   getBookContentById,
+// };
+
+const getBookContentById = async (bookId, currentPage, contentId) => {
+  // const book = data.book.find((b) => b.id == bookId);
+
+  const { data: book } = await supabase.from("Book").select("*").eq("id", bookId);
+
   if (!book) {
     return { error: "Cannot Find book" };
   }
-
-  const content = data.bookContent.filter((content) => content.bookId == bookId);
-  const bookDetail = data.book.find((book) => book.id == bookId);
+  const { data: content } = await supabase.from("BookContent").select("*").eq("bookId", bookId);
 
   if (content.length == 0) {
     return { error: "Cannot Find book content" };
